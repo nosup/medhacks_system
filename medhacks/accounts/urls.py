8a1e50f090aa7f3561f3f55a263855fe861c30b9
nosup <bryanki97@gmail.com>
2018-04-13 16:46:37 -0400

    login system, need to reset password

---
+++ medhacks/accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import login, logout
+urlpatterns = [
+    path('', views.home),
+    path('login/', login, {'template_name': 'accounts/login.html'}),
+    path('logout/', logout, {'template_name': 'accounts/logout.html'}),
+    path('register/', views.register, name='register'),
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password')
+]
