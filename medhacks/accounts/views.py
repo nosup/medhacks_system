3011c071fe15389f6a5dea80a23ba6977925f9f4
Lucy <lzhan115@jhu.edu>
2018-05-05 12:13:29 -0400

    send account created email

--- medhacks/accounts/views.py
+++ medhacks/accounts/views.py
@@ -5,10 +5,14 @@ from accounts.forms import (
 )
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth import login
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 from home.models import Application
+from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 def home(request):
     posts = Application.objects.filter(email=request.user.email)[:1]
@@ -19,28 +23,18 @@ def register(request):
     form = RegistrationForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
-            form.save()
+            user = form.save()
+            sender = settings.DEFAULT_FROM_EMAIL
+            recipient = [form.cleaned_data['email']]
+            subject = 'MedHacks Account'
+            content = render_to_string('accounts/account_created_email.html', {'user':user})
+            send_mail(subject,content,sender,recipient,fail_silently=False)
             return redirect(reverse('accounts:home'))
     return render(request, 'accounts/reg_form.html', {'form': form})
-
 
 def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
-
-
-# def edit_profile(request):
-#     if request.method == 'POST':
-#         form = EditProfileForm(request.POST, instance=request.user)
-#
-#         if form.is_valid():
-#             form.save()
-#             return redirect(reverse('accounts:view_profile'))
-#     else:
-#         form = EditProfileForm(instance=request.user)
-#         args = {'form': form}
-#         return render(request, 'accounts/edit_profile.html', args)
-
 
 def change_password(request):
     if request.method == 'POST':
@@ -49,6 +43,11 @@ def change_password(request):
         if form.is_valid():
             form.save()
             update_session_auth_hash(request, form.user)
+            # sender = settings.DEFAULT_FROM_EMAIL
+            # recipient = [form.user['email']]
+            # subject = 'Change Password'
+            # content = render_to_string('accounts/change_password_email.html')
+            # send_mail(subject,content,sender,recipient,fail_silently=False)
             return redirect(reverse('accounts:view_profile'))
         else:
             # TODO print error message: "incorrect password entry", etc
