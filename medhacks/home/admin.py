18909df40f230c2be4b8cc1c94efe9560d603fee
Matt <Matthew.sm.kim1@gmail.com>
2018-05-07 22:11:04 -0400

    education level now viewable in admin

--- medhacks/home/admin.py
+++ medhacks/home/admin.py
@@ -3,9 +3,10 @@ from home.models import Application
 from django.db.models.signals import post_save
 
 # Register your models here.
+# We see these in admin view under application
 class ApplicationAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'phone_number',
-    'city', 'state', 'country', 'gender',
+    'city', 'state', 'country', 'gender','education',
     'university', 'graduating_class', 'major', 'secondmajor', 'reimbursement',
     'user', 'attended', 'essay1', 'essay2', 'essay3', 'resume')
 
