0952a61d6b1327643cc72b6bc94b788807767352
nosup <bryanki97@gmail.com>
2018-06-09 12:09:36 -0400

    added databasedir to gitignore

--- medhacks/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-from home.views import HomeView
-
-
-app_name = 'home'
-urlpatterns = [
-    path('', HomeView.as_view(), name='home')
-]
--- medhacks/travel/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path, include
-from travel.views import TravelView
-
-
-app_name = 'home'
-urlpatterns = [
-    path('', TravelView.as_view(), name='travel')
-]
