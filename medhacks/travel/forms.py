d627cc9fb0615553a9a20089627626e759ebb1a1
jorilla1 <jorilla1@jhu.edu>
2018-06-10 16:42:40 -0400

    added button and changed text

--- medhacks/travel/forms.py
+++ medhacks/travel/forms.py
@@ -19,16 +19,10 @@ class TravelForm(forms.ModelForm):
     tupled_list_states = list(zip(states_list,states_list))
 
     CHOICESMEDHACKS=[('Yes','Yes'), ('No','No')]
-    Regional = """ Regional: includes the Northeast and some
-        parts of the South and Midwest where traveling is less than $50 (i.e.
-        BoltBus and MegaBus). You will only be reimbursed up to $50 and no more."""
-
-    Midwest = """ Midwest: includes most of the midwest where traveling exceeds $50. You will only
-        be reimbursed up to $100 and no more."""
-    West = """ West: includes West Coast where traveling significantly exceeds $50. You will only
-        be reimbursed up to $250 and no more."""
-    International = """ International: includes locations out of the
-        United States. International participants will receive up to $250 on a case by case basis."""
+    Regional = """ Regional"""
+    Midwest = """ Midwest"""
+    West = """ West"""
+    International = """ International"""
 
     CHOICES_TRAVEL=[('R', Regional), ('MW', Midwest), ('W', West), ('I', International)]
 
