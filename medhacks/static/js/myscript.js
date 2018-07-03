e595b1936ff987a9db03d37179a92001fc5ca63d
jorilla1 <jorilla1@jhu.edu>
2018-07-03 00:33:57 -0400

    changed font size

---
+++ medhacks/static/js/myscript.js
@@ -0,0 +1,15 @@
+$("#University").click(function(){
+    alert("ok it's good");
+});
+
+/*
+window.onload = function() {
+    if (window.jQuery) {
+        // jQuery is loaded
+        alert("Yeah!");
+    } else {
+        // jQuery is not loaded
+        alert("Doesn't Work");
+    }
+}
+*/
