1f2eaad73861608fc36a219ab7e812c1b8adbca0
nosup <bryanki97@gmail.com>
2018-07-22 16:15:39 -0400

    removed national and international options

--- medhacks/travel/forms.py
+++ medhacks/travel/forms.py
@@ -24,7 +24,7 @@ class TravelForm(forms.ModelForm):
     West = """ West"""
     International = """ International"""
 
-    CHOICES_TRAVEL=[('R', Regional), ('MW', Midwest), ('W', West), ('I', International)]
+    CHOICES_TRAVEL=[('R', Regional),]
 
     CHOICES_YN = (
         ('Y', 'Yes'),
