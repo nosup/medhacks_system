cb2df7293b71125b5edfa78a66bd1b1214c6085b
server <dennis.aldea@gmail.com>
2018-07-27 18:09:36 +0000

    added international button

--- medhacks/accounts/admin.py
+++ medhacks/accounts/admin.py
@@ -38,7 +38,7 @@ international_travel.short_description = "Accept TRAVEL: International"
 
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ('user', 'travel_reimbursement', 'travel_confirm', 'campus_ambassador', 'accepted', 'confirmation')
-    actions = [accept_hacker, reject_hacker, reject_travel, regional_travel, midwest_travel, west_travel,]
+    actions = [accept_hacker, reject_hacker, reject_travel, regional_travel, midwest_travel, west_travel, international_travel,]
 
     def user_info(self, obj):
         return obj.description
