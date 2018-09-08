a33b408e2f9a4c5f5bc2d0d289b5e058209e992b
Lucy <lzhan115@jhu.edu>
2018-09-08 01:04:54 -0400

    added new teams

--- medhacks/registerTeamApp/forms.py
+++ medhacks/registerTeamApp/forms.py
@@ -28,9 +28,10 @@ class SelectTeamForm(forms.ModelForm):
     'Humpty Dumpties ','Mira','Deacons','AI Socrates ','NutriDoc','TackTile','UR IN LIFE','Tumor Detectives ',
     'Blank','Scrubs','Rehabilita ','No More Headaches','ICU On The Map','Team Johnny','Byte Me','iBioMed',
     'HealthOverall','Spiro','Purple South African Flying Chinchillas','The Rices',
-    'The pirates','HTNhelp','E-connect','Neurons','Midnight Mavericks','Bandwidth','Full Source',
+    'The pirates','HTNhelp','E-connect','Neurons','Midnight Mavericks','Bandwidth','Flow Source',
     'Glove On','Stimtooth','Scribr','Uprising','E.P.I.C','Klear','JERS','Parachi','Spaghetti','TEAM-NAME?',
-    'Groceries','Melanotix','Scarlet Screw','5 Timmies','Medplex','Fugal','TBD','Just Pick Something','MedCircle','KALE']
+    'Groceries','Melanotix','Scarlet Screw','5 Timmies','Medplex','Fugal','TBD','Just Pick Something','MedCircle','KALE','BaLITmore',
+    'WikiMed','Scribbz']
     teams.sort()
     CHOICES_TEAMS = list(zip(teams, teams))
 
